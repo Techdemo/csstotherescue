8f8c0f41c8d5f71328472dce47a1a82c1b8fc2fe
Marcel <fleuren.marcel@gmail.com>
2019-02-28 14:48:43 +0100

    edited some styles

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ path.style.strokeDasharray = pathLength + ' ' + pathLength;
 path.style.strokeDashoffset = pathLength;
 path.getBoundingClientRect();
 window.addEventListener("scroll", function (e) {
-    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight);
     var drawLength = pathLength * scrollPercentage;
     path.style.strokeDashoffset = pathLength - drawLength;
     if (scrollPercentage >= 0.99) {
